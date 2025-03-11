bc193ffce64ecee8f7cfda91df9792a0aec20f33
Rafael Rossano <rafaelrossanopo@gmail.com>
2025-03-11 06:14:12 -0300

    -

--- bot/handlers.py
+++ bot/handlers.py
@@ -4,7 +4,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext, CallbackQueryHandler
 
 from bot.config import PERMISSIONED_GROUP_ID
-from bot.utils import user_is_group_admin, format_chat_object
+from bot.utils import user_is_group_admin, format_chat_object, sanitize_role_name
 from data.database import Database
 
 # Configuração do controlador da base de dados
@@ -98,24 +98,28 @@ async def apelidar(update: Update, context: CallbackContext) -> None:
         
     if not user_id:
         await update.message.reply_text("O usuário especificado não existe. (Tratar erro: nome passado errado ou nome correto, mas bot não reconheceu.)")
-
-    await bot.promote_chat_member(
-        chat_id=CHAT_ID,
-        user_id=user_id,
-        can_change_info=True,
-        can_delete_messages=False,
-        can_invite_users=False,
-        can_restrict_members=False,
-        can_pin_messages=False,
-        can_promote_members=False
-    )
-
-    await bot.set_chat_administrator_custom_title(
-        chat_id=CHAT_ID,
-        user_id=user_id,
-        custom_title=custom_title
-    )
     
+    
+    # Waiting for the complete procedure definition
+
+    # await bot.promote_chat_member(
+    #     chat_id=CHAT_ID,
+    #     user_id=user_id,
+    #     can_change_info=True,
+    #     can_delete_messages=False,
+    #     can_invite_users=False,
+    #     can_restrict_members=False,
+    #     can_pin_messages=False,
+    #     can_promote_members=False
+    # )
+
+    # await bot.set_chat_administrator_custom_title(
+    #     chat_id=CHAT_ID,
+    #     user_id=user_id,
+    #     custom_title=custom_title
+    # )
+    
+    db_controller.insert_member_in_division(user_id, username, custom_title)
     await update.message.reply_text(f"✏️ Apelido <b>{custom_title}</b> atribuido ao usuário <b>{username}</b>.", parse_mode="HTML")
     logging.info(f"Apelido {custom_title} atribuido ao usuário {username} > Responsável: @{update.effective_user.username} | Chat: {format_chat_object(update)}")
     
@@ -127,7 +131,8 @@ async def cargo(update : Update, context : CallbackContext):
         logging.info(f"Usuário {update.effective_user.username} tentou utilizar o comando /cargo fora de um grupo autorizado | Chat: {format_chat_object(update)}")
         return
     username = context.args[0]
-    role_name = " ".join(context.args[1:])
+    role_name_input = " ".join(context.args[1:])
+    role_name = sanitize_role_name(role_name_input)
     
     bot = context.bot
     
@@ -142,23 +147,30 @@ async def cargo(update : Update, context : CallbackContext):
         await update.message.reply_text("O usuário especificado não existe. (Tratar erro: nome passado errado ou nome correto, mas bot não reconheceu.)")
 
     role_permissions = db_controller.get_role_permissions(role_name)
-    
-    await bot.promote_chat_member(
-        chat_id=CHAT_ID,
-        user_id=user_id,
-        can_change_info=role_permissions['can_change_info'],
-        can_delete_messages=role_permissions['can_delete_messages'],
-        can_invite_users=role_permissions['can_invite_users'],
-        can_restrict_members=role_permissions['can_restrict_members'],
-        can_pin_messages=role_permissions['can_pin_messages'],
-        can_promote_members=role_permissions['can_promote_members']
-    )
+    try:
+        await bot.promote_chat_member(
+            chat_id=CHAT_ID,
+            user_id=user_id,
+            can_change_info=role_permissions['can_change_info'],
+            can_delete_messages=role_permissions['can_delete_messages'],
+            can_invite_users=role_permissions['can_invite_users'],
+            can_restrict_members=role_permissions['can_restrict_members'],
+            can_pin_messages=role_permissions['can_pin_messages'],
+            can_promote_members=role_permissions['can_promote_members']
+        )
 
-    await bot.set_chat_administrator_custom_title(
-        chat_id=CHAT_ID,
-        user_id=user_id,
-        custom_title=role_name
-    )
+        await bot.set_chat_administrator_custom_title(
+            chat_id=CHAT_ID,
+            user_id=user_id,
+            custom_title=role_name
+        )
+        
+        db_controller.update_member_role(username, db_controller.get_role_id(role_name))
+        
+        logging.info(f"Cargo {role_name} atribuído a {username} > Responsável: @{update.effective_user.username} | Chat: {format_chat_object(update)}")
+    except Exception:
+        await update.message.reply_text(f"<b>❌ Cargo inserido é inválido. Tente novamente.</b>\n<i>Caso isso seja um erro, contate a Equipe de Desenvolvedores, através da liderança da divisão.</i>", parse_mode="HTML")
+        logging.info(f"@{update.effective_user.username} utilizou um nome de cargo inválido ({role_name_input} > {role_name})| Chat: {format_chat_object(update)}")
 
 def message_is_on_group(chatId: float) -> bool:
     """
--- data/database.py
+++ data/database.py
@@ -8,6 +8,16 @@ class Database:
         self._init_user_table()
         self._init_chats_table()
         
+    def _init_division_database(self):
+        """
+        Inicilização e configuração inicial de tabelas básicas para testes.
+        """
+        self._create_division_table("em")
+        db.create_role('auxiliar', 1, 0, 1, 0, 1, 0)
+        db.create_role('sublider', 1, 1, 1, 0, 1, 0)
+        db.create_role('vicelider', 1, 1, 1, 1, 1, 1)
+        db.create_role('lider', 1, 1, 1, 1, 1, 1)
+        
     def _init_user_table(self):
         try:
             self.cursor.execute(f"""
@@ -85,6 +95,95 @@ class Database:
         except Exception as err:
             print(f"Error fetching all chats ->\n{err}")
             return []
+        
+    def _create_division_table(self, table_name : str) -> None:
+        try:
+            self.cursor.execute(f"""
+                CREATE TABLE IF NOT EXISTS {table_name} (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_id INTEGER UNIQUE,
+                    username TEXT,
+                    nickname TEXT,
+                    role INTEGER
+                )
+            """)
+            self.conn.commit()
+        except Exception as err:
+            print(f"Error creating division table ->\n{err}")
+            
+        try:
+            self.cursor.execute(f"""
+                CREATE TABLE IF NOT EXISTS {table_name}_roles (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    role_name TEXT,
+                    can_change_info INTEGER,
+                    can_delete_messages INTEGER,
+                    can_invite_users INTEGER,
+                    can_restrict_members INTEGER,
+                    can_pin_messages INTEGER,
+                    can_promote_members INTEGER
+                )
+            """)
+            self.conn.commit()
+        except Exception as err:
+            print(f"Error creating division table ->\n{err}")
+            
+    def create_role(self, role_name, info, delete, invite, rescrict, pin, promote):
+        try:
+            self.cursor.execute(f"INSERT OR IGNORE INTO em_roles (role_name, can_change_info, can_delete_messages, can_invite_users, can_restrict_members, can_pin_messages, can_promote_members) VALUES (?, ?, ?, ?, ?, ?, ?);", (role_name, info, delete, invite, rescrict, pin, promote))
+            self.conn.commit()
+        except Exception as err:
+            print(f"Error creating role ->\n{err}")
+            
+    def get_role_permissions(self, role_name):
+        try:
+            self.cursor.execute("SELECT can_change_info, can_delete_messages, can_invite_users, can_restrict_members, can_pin_messages, can_promote_members FROM guias_roles WHERE role_name=?", (role_name,))
+            result = self.cursor.fetchall()
+
+            if result:
+                permissions = [
+                    {
+                        "can_change_info": True if row[0] == 1 else False,
+                        "can_delete_messages": True if row[1] == 1 else False,
+                        "can_invite_users": True if row[2] == 1 else False,
+                        "can_restrict_members": True if row[3] == 1 else False,
+                        "can_pin_messages": True if row[4] == 1 else False,
+                        "can_promote_members": True if row[5] == 1 else False
+                    }
+                    for row in result
+                ]
+                return permissions[0]
+            else:
+                return None
+        except Exception as err:
+            print(f"Error getting role permissions ->\n{err}")
+            
+    def insert_member_in_division(self, user_id : id, username : str, nickname : str, table_name : str = "em"):
+        try:
+            self.cursor.execute(f"INSERT OR IGNORE INTO {table_name} (user_id, username, nickname, role) VALUES (?, ?, ?, ?);", (user_id, username, nickname, 0))
+            self.conn.commit()
+        except Exception as err:
+            print(f"Error inserting member in division ->\n{err}")
+            
+    def update_member_role(self, username : str, new_role_id : int, table_name : str = "em"):
+        try:
+            self.cursor.execute(f"UPDATE {table_name} SET role = ? WHERE username = ?", (new_role_id, username))
+            self.conn.commit()
+        except Exception as err:
+            print(f"Error updating member role ->\n{err}")
+            
+    def get_role_id(self, role_name : str, table_name : str = "em"):
+        try:
+            self.cursor.execute(f"SELECT id FROM {table_name}_roles WHERE role_name=?", (role_name,))
+            result = self.cursor.fetchone()
+        
+            if result:
+                return result[0]  
+            else:
+                return None 
+        except Exception as err:
+            print(f"Error getting role id ->\n{err}")
             
     def close(self):
         self.conn.close()
+        
